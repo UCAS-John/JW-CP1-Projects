3d07ddc6cfb985754b294c615fb4815a6303edaa
UCAS-John <john.wangwang@ucas-edu.net>
2024-09-03 09:36:13 -0600

    whoAreYou Project

---
+++ whoAreYou/main.py
@@ -0,0 +1,7 @@
+#John Wangwang whoAreYou
+
+name = input("Name: ")
+age = input("Age: ")
+color = input("Favorite Color: ")
+
+print("Hello", name, "! You are ", age, "years old, and your favorite color is", color, ".")
