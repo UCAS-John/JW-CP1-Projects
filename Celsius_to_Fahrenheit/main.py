d71badc881a06765ccb1f3a82341c0743b6e894c
UCAS-John <john.wangwang@ucas-edu.net>
2024-09-05 09:49:20 -0600

    Celsius to Fahrenheit

---
+++ Celsius_to_Fahrenheit/main.py
@@ -0,0 +1,8 @@
+#John Wangwang Celsius to Fahrenheit
+
+celsius = int(input("Enter Celsius temperature: "))
+
+fahrenheit = str((celsius * 9/5)+32)
+
+print("Celsius:", celsius)
+print("Fahrenhite:", fahrenheit)
