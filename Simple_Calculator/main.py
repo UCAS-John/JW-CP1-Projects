a196ca7845e9b5669406201fc6d055bce93087fd
UCAS-John <john.wangwang@ucas-edu.net>
2024-09-11 09:40:22 -0600

    Simple calculator

--- Simple_Calculator/main.py
+++ Simple_Calculator/main.py
@@ -1,5 +1,9 @@
 # John Wangwang Simple Calculator
 
+text = "Your answer is"
+x = None
+y = None
+decision = None
 x = float(input("Enter your first Number: "))
 y = float(input("Enter your Second Number: "))
 
@@ -7,18 +11,18 @@ print("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Modulus\n6.Exp
 decision = int(input("Enter the Number of your desire operator: "))
 
 if decision == 1:
-    print(x+y)
+    print(text, x+y)
 elif decision == 2:
-    print(x-y)
+    print(text, x-y)
 elif decision == 3:
-    print(x*y)
+    print(text, x*y)
 elif decision == 4:
-    print(x/y)
+    print(text, x/y)
 elif decision == 5:
-    print(x%y)
+    print(text, x%y)
 elif decision == 6:
-    print(x**y)
+    print(text, x**y)
 elif decision == 7:
-    print(x//y)
+    print(text, x//y)
 else:
     print("Operator number Error")
