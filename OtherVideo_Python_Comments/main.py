4496a6eacade797b19b40e87d54b170ffb097f3c
UCAS-John <john.wangwang@ucas-edu.net>
2024-09-27 09:32:28 -0600

    OtherVideo: Python Comments

---
+++ OtherVideo_Python_Comments/main.py
@@ -0,0 +1,15 @@
+#John Wangwang OtherVideo: Python Comments
+
+#-------------------------------------------------------------------------------------------------------------------
+# Name:        User enter their name
+# Purpose:     Collect and store username then print out the name
+#
+# Author:      John Wangwang
+#
+# Created:     27/9/2024
+# Licence:     (None)
+#-------------------------------------------------------------------------------------------------------------------
+
+name = input("Enter your name >>>") #Ask user for their name and store it
+
+print(f"hello, {name}") #Displays the string "hello" then the name
