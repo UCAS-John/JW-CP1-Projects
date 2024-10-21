c0493f9c80b8f4e25f43d105986b9a21df7d12ee
UCAS-John <john.wangwang@ucas-edu.net>
2024-10-21 09:20:13 -0600

    Count up and down

---
+++ Count up and down/main.py
@@ -0,0 +1,14 @@
+#John Wangwang SkillPractice: Count up and down
+
+def main():
+
+    #Count up from 0 to 19
+    for i in range(0,20):
+        print(i)
+    
+    #Count down from 20 to 0
+    for i in (range(20, -1, -1)):
+        print(i)
+
+if __name__ == "__main__":
+    main()
