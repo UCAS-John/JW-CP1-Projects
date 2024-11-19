797869dd0df0c49aad8934e7a320ce5b29ef24fc
UCAS-John <john.wangwang@ucas-edu.net>
2024-11-19 09:21:41 -0700

    RAID: Simple Histogram

---
+++ Simple Histogram/main.py
@@ -0,0 +1,30 @@
+
+def histogram(number: list):
+    for (i, num) in enumerate(number):
+        print(f"{i}:", end = "")
+        for j in range(num):
+            print("*", end = "")
+        print("")
+
+def main():
+
+    while True:
+ 
+        num_input = input("Enter at least 6 number seperated by space: ")  
+        
+        number = num_input.split()  
+        number = [int(num) for num in number]  
+
+        if not all(isinstance(num, int) for num in number):
+            print("Please Enter an integers!")
+            continue
+        if len(number) < 6:
+            print("Please Enter at least 6 number!")
+            continue
+        else:
+            break
+
+    histogram(number)
+
+if __name__ == "__main__":
+    main()
